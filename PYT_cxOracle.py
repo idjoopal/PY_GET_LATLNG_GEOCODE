990dfb4c7cf6240249e887d7c8dd706cd37b9643
JuHyeon Kim <idjoopal@naver.com>
2020-10-06 10:14:50 +0900

    Create PYT_cxOracle.py

---
+++ PYT_cxOracle.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import cx_Oracle
+import os
+import datetime
+from dateutil.relativedelta import *
+import calendar
+######################################################
+# variables
+host = 'db-ip-address'
+port = 8888 # your db port
+server = 'db-sid'
+user = 'db-username'
+password = 'db-pwd'
+######################################################
+# funtion : change pandas Dataframe to list
+def df_to_lst(DF):
+    DF = pd.DataFrame(DF)
+    return DF.values.tolist()
+######################################################
+# oracle encoding problem "hangeul(korean)"
+os.environ["NLS_LANG"] = ".AL32UTF8"
+
+# cx_Oracle connect
+dsn = cx_Oracle.makedsn(host, port, sid)
+conn = cx_Oracle.connect(user, password, dsn)
+cur = conn.cursor()
+
+# select query
+query = 'SELECT * FROM Table'
+DF_SRC = pd.read_sql(sql=query,con=conn)
+
+# delete query
+query = 'DELETE * FROM Table'
+cur.execute(query)
+conn.commit()
+
+# insert query
+query = 'INSERT * INTO Table'
+RST_LST = df_to_lst(DF_RST)
+cur.executemany(query, RST_LST)
+conn.commit()
+
+# disconnect
+conn.close()
+######################################################
